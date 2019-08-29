c45da3d9c05d221344de3d9ceb6f14b4408872c4
David Page <dpage@expansiagroup.com>
2019-08-29 08:48:07 -0400

    version 1.2.3

--- lib/widgetizer/widgetizer.py
+++ lib/widgetizer/widgetizer.py
@@ -7,8 +7,6 @@ class Widgetizer:
         self.widget = ''.join(['(ಠ_ಠ){}(ಠ_ಠ)\n'.format(self.widget_material) \
         for _ in range(self.secret_sauce)])
 
-        print(self.widget)
-
     def save_widget(self):
 
         with open('{}/widget.store'.format(PATH_TO_DATA_FILE), 'a') as outfile:
